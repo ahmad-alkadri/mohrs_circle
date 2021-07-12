09de14eeef1b9369121c27252b387668c7278751
Ahmad Alkadri <ahmad.alkadri@outlook.com>
2021-07-12 15:27:30 +0200

    Tidying up the script

--- streamlit_app_bokeh.py
+++ streamlit_app_bokeh.py
@@ -15,9 +15,15 @@ from user_funcs import mohr_c, c_array, X_Y
 st.title("Mohr's Circle App")
 
 # Make the sidebar with the inputs
-stress_x = st.sidebar.number_input("stress in x", value=2.0, step=0.5)
-stress_y = st.sidebar.number_input("stress in y", value=5.0, step=0.5)
-shear = st.sidebar.number_input("shear xy", value=4.0, step=0.5)
+stress_x = st.sidebar.number_input("stress in x", 
+    value=2.0, 
+    step=0.5)
+stress_y = st.sidebar.number_input("stress in y", 
+    value=5.0, 
+    step=0.5)
+shear = st.sidebar.number_input("shear xy", 
+    value=4.0, 
+    step=0.5)
 
 # find center and radius
 C, R = mohr_c(stress_x, stress_y, shear)
@@ -26,7 +32,9 @@ C, R = mohr_c(stress_x, stress_y, shear)
 circle_x, circle_y = c_array(C, R)
 
 # build arrays to plot line through the circle
-X, Y = X_Y(stress_x, stress_y, shear)
+X, Y = X_Y(stress_x, 
+    stress_y, 
+    shear)
 
 st.sidebar.markdown(f"max stress = {round(C+R,2)}")
 st.sidebar.markdown(f"min stress = {round(C-R,2)}")
@@ -43,8 +51,15 @@ p = figure(
     tools="pan,reset,save,wheel_zoom",
 )
 
-p.line(circle_x, circle_y, color="#1f77b4", line_width=3, line_alpha=0.6)
-p.line(X, Y, color="#ff7f0e", line_width=3, line_alpha=0.6)
+p.line(circle_x, 
+    circle_y, 
+    color="#1f77b4", 
+    line_width=3, 
+    line_alpha=0.6)
+p.line(X, Y, 
+    color="#ff7f0e", 
+    line_width=3, 
+    line_alpha=0.6)
 
 p.xaxis.fixed_location = 0
 p.yaxis.fixed_location = 0
